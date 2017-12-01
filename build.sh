72d8b67939e6035f8f8aaa0b9ca89810e4c7b535
Sascha <sascha.trifunovic@gmail.com>
2017-12-01 20:05:17 +0100

    Move main documentation into "documentation" folder

--- build.sh
+++ build.sh
@@ -1,36 +1,36 @@
 #! /bin/bash
 
 # construct doc.md
-cat intro.md > doc.md
-cat misc/supported_protocols.md >> doc.md
-cat clients/ruby/ruby.md >> doc.md
-cat clients/ruby/rails3_4.md >> doc.md
-cat clients/ruby/rails2.md >> doc.md
-cat clients/ruby/puma.md >> doc.md
-cat clients/ruby/rack.md >> doc.md
-cat clients/python/python.md >> doc.md
-cat clients/python/django.md >> doc.md
-cat clients/php/php.md >> doc.md
-cat clients/php/wordpress.md >> doc.md
-cat clients/php/cake_php.md >> doc.md
-cat clients/php/symfony2.md >> doc.md
-cat clients/php/laravel.md >> doc.md
-cat clients/js/node_js.md >> doc.md
-cat clients/java/java.md >> doc.md
-cat clients/supported_clients.md >> doc.md
-cat clients/sample_apps.md >> doc.md
-cat misc/local_usage.md >> doc.md
-cat analytics/analytics.md >> doc.md
-cat analytics/api.md >> doc.md
-cat analytics/new_relic.md >> doc.md
-cat analytics/credentials.md >> doc.md
-cat analytics/disabled_caches.md >> doc.md
-cat misc/encrypted_connections.md >> doc.md
-cat misc/changing_plans.md >> doc.md
-cat misc/using_memcachier.md >> doc.md
-cat misc/value_limit.md >> doc.md
-cat misc/localhost_error.md >> doc.md
-cat misc/support.md >> doc.md
+cat documentation/intro.md > doc.md
+cat documentation/misc/supported_protocols.md >> doc.md
+cat documentation/clients/ruby/ruby.md >> doc.md
+cat documentation/clients/ruby/rails3_4.md >> doc.md
+cat documentation/clients/ruby/rails2.md >> doc.md
+cat documentation/clients/ruby/puma.md >> doc.md
+cat documentation/clients/ruby/rack.md >> doc.md
+cat documentation/clients/python/python.md >> doc.md
+cat documentation/clients/python/django.md >> doc.md
+cat documentation/clients/php/php.md >> doc.md
+cat documentation/clients/php/wordpress.md >> doc.md
+cat documentation/clients/php/cake_php.md >> doc.md
+cat documentation/clients/php/symfony2.md >> doc.md
+cat documentation/clients/php/laravel.md >> doc.md
+cat documentation/clients/js/node_js.md >> doc.md
+cat documentation/clients/java/java.md >> doc.md
+cat documentation/clients/supported_clients.md >> doc.md
+cat documentation/clients/sample_apps.md >> doc.md
+cat documentation/misc/local_usage.md >> doc.md
+cat documentation/analytics/analytics.md >> doc.md
+cat documentation/analytics/api.md >> doc.md
+cat documentation/analytics/new_relic.md >> doc.md
+cat documentation/analytics/credentials.md >> doc.md
+cat documentation/analytics/disabled_caches.md >> doc.md
+cat documentation/misc/encrypted_connections.md >> doc.md
+cat documentation/misc/changing_plans.md >> doc.md
+cat documentation/misc/using_memcachier.md >> doc.md
+cat documentation/misc/value_limit.md >> doc.md
+cat documentation/misc/localhost_error.md >> doc.md
+cat documentation/misc/support.md >> doc.md
 # check doc.md for dangling links
 ./check_links.py
 # create direct and heroku version of doc.md
@@ -38,7 +38,7 @@ cat misc/support.md >> doc.md
 ./flavor.py doc.md doc.heroku.md heroku
 # overwrite heroku doc
 rm -f memcachier.md
-devcenter pull https://devcenter.heroku.com/articles/memcachier
+devcenter pull memcachier
 mv memcachier.md memcachier.old.md
 head -n 4 memcachier.old.md > memcachier.md
 cat doc.heroku.md >> memcachier.md
